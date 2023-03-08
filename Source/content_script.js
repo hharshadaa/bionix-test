cd6f9220f83cec7eec9cb435fb52c201c13fa271
Joestar-V <55765777+Joestar-V@users.noreply.github.com>
2023-03-08 13:40:10 -0800

    Update content_script.js

--- Source/content_script.js
+++ Source/content_script.js
@@ -39,11 +39,8 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
-	
+	v = v.replace(/\bbeautiful\b/g, "harshada");
+	v = v.replace(/\bBeautiful\b/g, "Harshada
 	textNode.nodeValue = v;
 }
 
